5ae623ef033ebf787dd5f4064baeee5edd92c4d6
Nadeem Anwar <57771677+nadeem-ds@users.noreply.github.com>
2024-12-27 23:07:30 +0530

    first commit

---
+++ 01Nadeem.java
@@ -0,0 +1,20 @@
+package nad;
+import java.lang.*;
+import java.util.Scanner;
+
+public class Nadeem {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        madam();
+    }
+    public static void madam(){
+        System.out.println("first class");
+    }
+
+
+
+
+}
+
+
+
